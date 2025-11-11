ab46f9058941df2734a20f418588fb32ca6df9bb
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-11-11 13:28:29 +0530

    added

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid email or password" });
     }   
 
-    const token = jwt.verify({ id: user._id }, process.env.JWT_SECRET , {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET , {
         expiresIn : '1d'
     });
 
