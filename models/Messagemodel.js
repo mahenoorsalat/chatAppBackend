dbc38d24ac19df5f0fd16f37823f2ec0e3203151
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-11-13 12:15:35 +0530

    added

---
+++ models/Messagemodel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUserProfile , updateUserProfile } from '../controllers/authController.js';
+import { getAllUsers, getUserProfile , updateUserProfile } from '../controllers/authController.js';
 import { ProtectRoute } from '../middlewares/authMiddlewares.js';
 
 const router = express.Router();
 
 router.get('/profile' , ProtectRoute  , getUserProfile)
 router.post('/profile' , ProtectRoute  , updateUserProfile)
+router.get('/all' , ProtectRoute  , getAllUsers)
 
 export default router;
